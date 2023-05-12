85e846a47a93d4d21d87124c8155fe6021a78ecd
shant-k <shantk650@gmail.com>
2023-05-12 17:32:20 +0300

    test

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-const express=require ("express")
-const app= express()
-require('dotenv').config();
-const colors = require("colors")
-const connectDB=require("./config/db")
-const port = process.env.PORT ||5000;
-connectDB()
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const colors = require("colors");
+console.log(colors);
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+connectDB();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(port,console.log(`server running on port ${port}`))
+app.listen(port, console.log(`server running on port ${port}`));
